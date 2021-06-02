f7d6e51bcf258bc29696c49d5c041dc10a91456a
dawiteu <dawid@tararuj.be>
2021-06-02 18:46:12 +0200

    recherche dynamique icones service

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -3,15 +3,46 @@ $(function(){
 
 
     // resultats recherche Icones dans la DB. 
-    
-    if($("#results")){
-    let newicon; 
-        $(".selecticon").on("dblclick", function() {
-            newicon = $(this).attr('data');
-            navigator.clipboard.writeText(newicon);
+    let tab = []; 
 
-            if(navigator.clipboard.writeText(newicon)){
-                window.close();
+    if($("#query")){
+        $("#query").on("input", function(){
+            let val = $("#query").val();
+            
+            if(val.length > 0 ){
+                $("#results").html(""); 
+                let res = tab.filter(item => item.indexOf(val) > -1); 
+                //console.log(res);
+                $("#counts").html(res.length);
+                res.forEach(item => {
+                    $("#results").append(`<div class='m-5 ico bg-red-300 w-max flex flex-col justify-center text-center selecticon' data='${item}'> <i style="font-size:4rem;" class="${item} bg-gray-200" title="${item}"></i></div>`); 
+                });
+            }
+        });
+    }
+
+    if($("#results")){
+
+        let childs = $("#results").children(); 
+
+        childs.each(function (i, child){
+            tab[i] = child.getAttribute('data'); 
+        });
+
+        console.log(tab); 
+    
+        $(".selecticon").on("click", function() {
+            let newicon = $(this).attr('data');
+            let conf=confirm(`Confirmez-vous le choix de cette icone? (${newicon}) `);
+            if(conf){
+                navigator.clipboard.writeText(newicon);
+    
+                let x =setTimeout(() => {
+                    if(navigator.clipboard.writeText(newicon)){
+                        clearTimeout(x);
+                        window.close();
+                    }
+                }, 300);
             }
         });
     }
@@ -19,6 +50,7 @@ $(function(){
     if($("#setnewicon")){ 
         window.onfocus = function() { 
             navigator.clipboard.readText().then(function(text){
+                console.log(text);
                 let auth = ["fas", "far", "fab"]; 
                 let parse = text.split(" "); 
 
--- resources/views/admin/services/icones.blade.php
+++ resources/views/admin/services/icones.blade.php
@@ -5,7 +5,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-between">
                         <h3 class="text-xl m-4 text-center md:text-left">Liste des icones: </h3> 
-                        <p class="m-3 text-right text-gray-500">Icones disponibles: {{count($allservs)}}</p>
+                        <p class="m-3 text-right text-gray-500">Icones disponibles: <span id="counts">{{count($allservs)}}</span></p>
                     </div>
                     <div class="my-4">
                         <input  class="w-full" 
@@ -16,10 +16,9 @@
                     </div>
                     <div id="results" class="flex flex-wrap">
                         @for ($i = 0; $i < count($allservs); $i++)
+                            {{-- <script>tab.push( @php $allservs[$i] @endphp )</script>  --}}
                             <div class="m-5 ico w-max flex flex-col justify-center text-center selecticon" data="{{$allservs[$i]}}">
-
-                                <i style="font-size:4rem;" class="{{ $allservs[$i]}} bg-gray-200" title="{{$allservs[$i]}}"></i>
-
+                                <i style="font-size:4rem;" class="{{ $allservs[$i] }} bg-gray-200" title="{{$allservs[$i]}}"></i>
                             </div>
                         @endfor
                     </div>
