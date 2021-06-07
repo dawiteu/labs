70ab0af36549cdbfe26379254220965a5ba72c44
dawiteu <dawid@tararuj.be>
2021-06-07 12:14:42 +0200

    CEO OK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -191,18 +191,32 @@
 
         <div class="row">
             @for ($i = 0; $i < 2; $i++)
-                <!-- single member -->
-                <div class="col-sm-4">
-                    <div class="member {{$i}}">
-                        <img src={{ $i == 1 ? $teamceo != "noceo" ? asset($teamceo->image) : asset("img/def/noav2.png") : asset($teamgro[$i]->image) }} alt=""> 
-                        <h2>     {{ $i == 1 ? $teamceo != "noceo" ? $teamceo->prenom . " " . $teamceo->nom : "noav " : $teamgro[$i]->prenom . " " . $teamgro[$i]->nom}}</h2>
-                        <h3>     {{ $i == 1 ? $teamceo != "noceo" ? $teamceo->poste->nom : "pas de ceo dispo " : $teamgro[$i]->poste->nom }}</h3>
-                    </div>
-                </div>
                 @if ($i == 1) {{-- LE MILIEU, CEO--}}
-                    
+                    <!-- single member -->
+                    <div class="col-sm-4">
+                        <div class="member {{$i}} CEO">
+                            <img src={{  $teamceo != "noceo" ? asset($teamceo->image) : asset("img/def/noav2.png") }} alt=""> 
+                            <h2>     {{  $teamceo != "noceo" ? $teamceo->prenom . " " . $teamceo->nom : "noav " }}</h2>
+                            <h3>     {{  $teamceo != "noceo" ? $teamceo->poste->nom : "pas de ceo dispo "  }}</h3>
+                        </div>
+                    </div>
+                    <!-- single member -->
+                    <div class="col-sm-4">
+                        <div class="member {{$i}}">
+                            <img src="{{asset($teamgro[$i]->image) }}" alt="{{ $teamgro[$i]->prenom . " " . $teamgro[$i]->nom}}"> 
+                            <h2> {{ $teamgro[$i]->prenom . " " . $teamgro[$i]->nom}}</h2>
+                            <h3> {{ $teamgro[$i]->poste->nom }}</h3>
+                        </div>
+                    </div>
                 @else
-                    
+                    <!-- single member -->
+                    <div class="col-sm-4">
+                        <div class="member {{$i}}">
+                            <img src="{{asset($teamgro[$i]->image) }}" alt="{{ $teamgro[$i]->prenom . " " . $teamgro[$i]->nom}}"> 
+                            <h2> {{ $teamgro[$i]->prenom . " " . $teamgro[$i]->nom}}</h2>
+                            <h3> {{ $teamgro[$i]->poste->nom }}</h3>
+                        </div>
+                    </div>
                 @endif
             @endfor
         </div>
